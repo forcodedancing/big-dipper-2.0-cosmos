44786eb48528c53e54333eb5c1cfc5441a8f3bd3
forcodedancing <just.haha.it@gmail.com>
2022-12-30 16:53:52 +0800

    add scripts

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -3,6 +3,9 @@
 BDJUNO=~/.bdjuno/
 GENESIS=$1
 
+export CGO_CFLAGS="-O -D__BLST_PORTABLE__"
+export CGO_CFLAGS_ALLOW="-O -D__BLST_PORTABLE__"
+
 echo "bdjuno home:"$BDJUNO
 echo "genesis:"$GENESIS
 
@@ -17,45 +20,48 @@ rm -rf $BDJUNO
 mkdir $BDJUNO
 cd $BDJUNO
 
-git clone github.com/forcodedancing/juno
-cd juno & git checkout ins
+git clone https://github.com/forcodedancing/juno.git
+cd $BDJUNO/juno && git checkout ins
 cd ../
 
-git clone github.com/forcodedancing/gaia
-cd gaia & git checkout ins
+git clone https://github.com/forcodedancing/gaia.git
+cd $BDJUNO/gaia && git checkout ins
 cd ../
 
-git clone github.com/forcodedancing/bdjuno
-cd bdjuno & git checkout ins
+git clone https://github.com/forcodedancing/bdjuno.git
+cd $BDJUNO/bdjuno && git checkout ins
 make install
 cd ../
 
-git clone github.com/forcodedancing/big-dipper-2.0-cosmos
-cd big-dipper-2.0-cosmos & git checkout ins
+git clone https://github.com/forcodedancing/big-dipper-2.0-cosmos.git
+cd $BDJUNO/big-dipper-2.0-cosmos && git checkout ins
 cd ../
 
 echo "4. docker compose up for postgresql and hasura"
 cd $BDJUNO
-cd big-dipper-2.0-cosmos/deployment/docker-cosmos.yml $BDJUNO/docker-cosmos.yml
-docker-compose stop
-docker volume rm hasura_db_data
+cp big-dipper-2.0-cosmos/deployment/docker-compose.yml $BDJUNO/docker-compose.yml
+docker-compose down
+sleep 10
+docker volume rm bdjuno_db_data
 docker-compose up -d
+sleep 10
 
-echo "5. create database and tables"
-PGPASSWORD=pass createdb -h localhost -p 5432 -U postgres bdjuno
-PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d bdjuno -f 00-cosmos.sql
-PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d bdjuno -f 01-auth.sql
-PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d bdjuno -f 02-bank.sql
-PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d bdjuno -f 03-staking.sql
-PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d bdjuno -f 04-consensus.sql
-PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d bdjuno -f 05-mint.sql
-PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d bdjuno -f 06-distribution.sql
-PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d bdjuno -f 07-pricefeed.sql
-PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d bdjuno -f 08-gov.sql
-PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d bdjuno -f 09-modules.sql
-PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d bdjuno -f 10-slashing.sql
-PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d bdjuno -f 11-feegrant.sql
-PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d bdjuno -f 12-upgrade.sql
+echo "5. create tables"
+cd $BDJUNO
+cd bdjuno/database/schema
+PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d postgres -f 00-cosmos.sql
+PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d postgres -f 01-auth.sql
+PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d postgres -f 02-bank.sql
+PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d postgres -f 03-staking.sql
+PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d postgres -f 04-consensus.sql
+PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d postgres -f 05-mint.sql
+PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d postgres -f 06-distribution.sql
+PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d postgres -f 07-pricefeed.sql
+PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d postgres -f 08-gov.sql
+PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d postgres -f 09-modules.sql
+PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d postgres -f 10-slashing.sql
+PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d postgres -f 11-feegrant.sql
+PGPASSWORD=pass psql -U postgres -h 127.0.0.1 -p 5432 -d postgres -f 12-upgrade.sql
 
 echo "6. prepare hasura metadata"
 cd $BDJUNO
@@ -64,10 +70,10 @@ hasura metadata apply --endpoint http://localhost:8080 --admin-secret myadminsec
 
 echo "7. start bdjuno"
 cd $BDJUNO
-cd big-dipper-2.0-cosmos/deployment/config.yaml $BDJUNO/config.yaml
+cp big-dipper-2.0-cosmos/deployment/config.yaml $BDJUNO/config.yaml
 cp $GENESIS $BDJUNO/genesis.json
 bdjuno parse genesis-file --genesis-file-path $BDJUNO/genesis.json
-nohup bdjuno start &
+bdjuno start
 
 echo "8. start web"
 cd $BDJUNO
